5b52c97bd27a4b8b1139dac6427770f3ef8bd11d
Rajarshi <rajarshidas47@gmail.com>
2024-01-07 12:47:00 +0530

    practiced Pair 7/1/2024

--- practice/pairPrac.cpp
+++ practice/pairPrac.cpp
@@ -1,10 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int main()
 {
-    
-    cout<<endl;
+    int n;
+    pair<int, int> p1 = {1, 2};
+    cout << p1.first << '\t' << p1.second << endl;
+
+    cout << "The enter size of the pair array  = ";
+    cin >> n;
+    pair<int, int> ar[n];
+    cout << endl
+         << "Enter values in the pair array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i].first;
+        cin >> ar[i].second;
+    }
+    cout << endl
+         << "Entered pairs:" << endl;
+    for (int i = 0; i < n; i++)
+        cout << ar[i].first << '\t' << ar[i].second << endl;
+    pair<int, int> p2(60, 20);
+    pair<int, int> p3;
+    p3 = make_pair(90, 60);
+    pair<pair<int, int>, pair<int, int>> p5;
+    p5 = {p2, p3};
+    cout << p5.first.first << '\t' << p5.first.second << '\t' << '\t' << p5.second.first << '\t' << p5.second.second << endl;
+
+    // traverse the pair array
+    cout << "Traversing the pair array:" << endl;
+    for (auto it : ar)
+        cout << it.first << '\t' << it.second << endl;
+    cout << "Manipulating the pair array:" << endl;
+    for (auto &it : ar)
+    {
+        it.first += 2;
+        it.second -= 5;
+    }
+    cout << "Traversing after ma manipulation the pair array:" << endl;
+    for (auto it : ar)
+        cout << it.first << '\t' << it.second << endl;
+
     return 0;
 }
